ede9a2c9301ca6b30e973cdbccdcfb50107b68b9
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-20 15:27:10 +0530

    ADDED: Colors logic, Passing data

--- ColorsApp/ColorsTableVC.swift
+++ ColorsApp/ColorsTableVC.swift
@@ -8,25 +8,46 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
-
+    var colors: [UIColor] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      
+      createRandomColors()
     }
-   
+    func createRandomColors(){
+        for _ in 0..<50{
+            colors.append(createRandomColor())
+        }
+    }
     
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in:0...1),
+                                  green: CGFloat.random(in:0...1),
+                                  blue: CGFloat.random(in:0...1),
+                                  alpha: 1)
+        
+        return randomColor
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! DetailsVC
+        destVC.color = sender as! UIColor
+    }
 }
 extension ColorsTableVC: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else{return UITableViewCell()}
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ToColorsDetailVc", sender: nil)
+        let color = colors[indexPath.row]
+        
+        performSegue(withIdentifier: "ToColorsDetailVc", sender: color)
     }
 }
--- ColorsApp/DetailsVC.swift
+++ ColorsApp/DetailsVC.swift
@@ -8,22 +8,9 @@
 import UIKit
 
 class DetailsVC: UIViewController {
-
+    var color: UIColor?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = color ?? .cyan
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
