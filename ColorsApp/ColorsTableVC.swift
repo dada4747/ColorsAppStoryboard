c5dedc9b68d7137c49d90577249c2ca6dfcaaf37
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-20 15:32:48 +0530

    REFACTORED: Code for random colors

--- ColorsApp/ColorsTableVC.swift
+++ ColorsApp/ColorsTableVC.swift
@@ -11,27 +11,17 @@ class ColorsTableVC: UIViewController {
     var colors: [UIColor] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
       createRandomColors()
     }
     func createRandomColors(){
         for _ in 0..<50{
-            colors.append(createRandomColor())
+            colors.append(.random())
         }
-    }
-    
-    func createRandomColor() -> UIColor {
-        let randomColor = UIColor(red: CGFloat.random(in:0...1),
-                                  green: CGFloat.random(in:0...1),
-                                  blue: CGFloat.random(in:0...1),
-                                  alpha: 1)
-        
-        return randomColor
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destVC = segue.destination as! DetailsVC
-        destVC.color = sender as! UIColor
+        destVC.color = (sender as! UIColor)
     }
 }
 extension ColorsTableVC: UITableViewDelegate,UITableViewDataSource{
