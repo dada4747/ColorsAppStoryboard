76fad29f5de024299aa7fbab4e9f34157fe5bbbb
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-20 14:31:40 +0530

    REFACTORED: remove temporary used button code, added table view, extension for VC extend delegate and datasource methods

--- ColorsApp/ColorsTableVC.swift
+++ ColorsApp/ColorsTableVC.swift
@@ -14,10 +14,19 @@ class ColorsTableVC: UIViewController {
 
       
     }
+   
     
-    @IBAction func tempButtonTapped(_ sender: Any) {
-        performSegue(withIdentifier: "ToColorsDetailVc", sender: nil)
+}
+extension ColorsTableVC: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorsDetailVc", sender: nil)
+    }
 }
